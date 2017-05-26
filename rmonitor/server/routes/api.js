2c2f8984f201f7fd33d01d69b88d5168c3a9a0b6
Filiphe <liphvf@gmail.com>
2017-05-26 12:33:28 -0300

    adicionando systeminformation

--- rmonitor/server/routes/api.js
+++ rmonitor/server/routes/api.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const sysinfo = require('systeminformation');
 const router = express.Router();
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
+
+router.get('/info', (req, res) => {
+  return sysinfo.cpu()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 });
 
 module.exports = router;
