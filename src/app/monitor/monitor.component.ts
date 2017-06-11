0ef0adfe735213ed5bb05973638d270a9b48b92e
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2017-06-11 17:36:00 -0300

    exemplo de diretivos usando o ngFor

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MonitorComponent implements OnInit {
 
   title: string;
+  monitores: string[] = ["CPU", "Memória"]
 
   constructor() {
     this.title = "Meu monitor"
