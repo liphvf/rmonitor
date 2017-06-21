4c71d11f43fb001f1dd700e444cdee71e9b20c62
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2017-06-21 18:28:55 -0300

    ajustando o grafico

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -1,6 +1,7 @@
 import { Iinfo } from './Iinfo';
 import { MonitorService } from './monitor.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BaseChartDirective } from "ng2-charts";
 
 @Component({
   selector: 'monitor',
@@ -8,74 +9,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monitor.component.css']
 })
 export class MonitorComponent implements OnInit {
-
+  @ViewChild(BaseChartDirective)
+  public chart: BaseChartDirective
+  
   info: Iinfo;
   memory: string
-  cpuLoad : string
+  cpuLoad: string
   errMessage: string = "";
   monitores: string[];
 
   constructor(private monitorService: MonitorService) {
   }
 
-  // // lineChart
-  // public lineChartData: Array<any> = [
-  //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-  //   { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
-  // ];
-  // public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  // public lineChartOptions: any = {
-  //   responsive: true
-  // };
-  // public lineChartColors: Array<any> = [
-  //   { // grey
-  //     backgroundColor: 'rgba(148,159,177,0.2)',
-  //     borderColor: 'rgba(148,159,177,1)',
-  //     pointBackgroundColor: 'rgba(148,159,177,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  //   },
-  //   { // dark grey
-  //     backgroundColor: 'rgba(77,83,96,0.2)',
-  //     borderColor: 'rgba(77,83,96,1)',
-  //     pointBackgroundColor: 'rgba(77,83,96,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(77,83,96,1)'
-  //   },
-  //   { // grey
-  //     backgroundColor: 'rgba(148,159,177,0.2)',
-  //     borderColor: 'rgba(148,159,177,1)',
-  //     pointBackgroundColor: 'rgba(148,159,177,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  //   }
-  // ];
-  // public lineChartLegend: boolean = true;
-  // public lineChartType: string = 'line';
+  // lineChart
+  public lineChartData: Array<any> = [
+    { data: [], label: 'Memória' },
+    { data: [], label: 'CPU' },
+  ];
+  public lineChartLabels: Array<any> = [];
+  public lineChartOptions: any = {
+    responsive: true
+  };
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    }
+  ];
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
 
-  // public randomize(): void {
-  //   let _lineChartData: Array<any> = new Array(this.lineChartData.length);
-  //   for (let i = 0; i < this.lineChartData.length; i++) {
-  //     _lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
-  //     for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-  //       _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-  //     }
-  //   }
-  //   this.lineChartData = _lineChartData;
-  // }
+  public addToChart(memory: number, cpu: number): void {
+    let _lineChartData = this.lineChartData;
+    _lineChartData[0].data.push(memory);
+    _lineChartData[1].data.push(cpu);
+    this.lineChartLabels.push('');
 
-  // // events
-  // public chartClicked(e: any): void {
-  //   console.log(e);
-  // }
+    this.lineChartData = _lineChartData;
 
-  // public chartHovered(e: any): void {
-  //   console.log(e);
-  // }
+    this.chart.chart.update();
+  }
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 
   ngOnInit() {
 
@@ -84,9 +78,10 @@ export class MonitorComponent implements OnInit {
       this.monitorService.getInfo().subscribe(
         response => {
           // this.info = response;
-          this.info  = response;
+          this.info = response;
           this.memory = response.Memory.used;
           this.cpuLoad = response.CpuLoad.avgload;
+          this.addToChart(response.Memory.used, response.CpuLoad.avgload);
           // console.log(response);
         }
       );
